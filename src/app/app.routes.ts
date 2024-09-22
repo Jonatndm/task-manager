0ca5f9f0c01433d65fd235eac59c4d4f93866152
Jonathan <81990209+Jonatndm@users.noreply.github.com>
2024-09-22 19:07:48 -0400

    Agregando login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { TaskComponent } from './task/task.component';
 import { LoginComponent } from './login/login.component';
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+  
+  
+  get(){
+    localStorage.getItem('username');
+  }
+  
+  onLogin(){
+    if(this.username && this.password){
+        localStorage.setItem('username', this.username);
+
+        this.router.navigate(['/tasks']);
+    }else {
+      alert('Por favor, ingresa un usuario y contrasena valida,');
+    }
+  }
 }
